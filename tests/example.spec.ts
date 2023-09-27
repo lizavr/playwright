4fc3930cbf1d2603887e8cffc6582c66eb336c40
lizavr <elizavetarabetskaya@gmail.com>
2023-09-27 16:06:39 +0400

    feat: pass the first test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,19 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('has button sign in', async ({ page }) => {
+  await page.goto('https://www.google.com/intl/en/gmail/about/');
+  const signInBtn = await page.locator('a:text("Sign in")');
   await page.screenshot({ path: 'screenshots/screenshot1.png' });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-  await page.screenshot({ path: 'screenshots/screenshot2.png' });
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Expect a button "Sign in" exist.
+  await expect(signInBtn).toBeVisible();
 });
