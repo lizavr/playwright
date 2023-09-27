b4165b0126f9f5f43878ae805a8c983f53fce176
lizavr <elizavetarabetskaya@gmail.com>
2023-09-27 15:32:48 +0400

    feat: add screenshots

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
+  await page.screenshot({ path: 'screenshots/screenshot1.png' });
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
@@ -12,7 +13,7 @@ test('get started link', async ({ page }) => {
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
-
+  await page.screenshot({ path: 'screenshots/screenshot2.png' });
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
